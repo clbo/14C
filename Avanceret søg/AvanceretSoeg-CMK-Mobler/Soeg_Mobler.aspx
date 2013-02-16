97373135dc0504d53268946b5ef45cd7eb914641
Claus <clb@rts.dk>
2013-02-16 11:49:14 +0100

    Avanseret søg repitition

---
+++ Avanceret søg/AvanceretSoeg-CMK-Mobler/Soeg_Mobler.aspx
@@ -0,0 +1,147 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true"
+    CodeFile="Soeg_Mobler.aspx.cs" Inherits="Soeg_Mobler" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+    <style type="text/css">
+        .style1
+        {
+            width: 500px;
+        }
+        .style2
+        {
+            height: 16px;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <table class="style1">
+        <tr>
+            <td>
+                <asp:Label ID="Label_varenummer" runat="server" Text="Vare nr."></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="TextBox_varenummer" runat="server"></asp:TextBox>
+            </td>
+            <td>
+                <asp:Button ID="Button_varenummer" runat="server" Text="Søg" 
+                    onclick="Button_varenummer_Click" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="Label_moebelserie" runat="server" Text="Møbelserie"></asp:Label>
+            </td>
+            <td>
+               
+                <asp:CheckBoxList ID="CheckBoxList_kategori" runat="server" 
+                    DataSourceID="SqlDataSource2" DataTextField="K_Navn" DataValueField="K_id" 
+                    RepeatColumns="2" RepeatLayout="Flow">
+                    
+                </asp:CheckBoxList>
+                <asp:SqlDataSource ID="SqlDataSource2" runat="server" 
+                    ConnectionString="<%$ ConnectionStrings:ConnectionString %>" 
+                    SelectCommand="SELECT [K_id], [K_Navn] FROM [Kategori]"></asp:SqlDataSource>
+            </td>
+            <td>
+                &nbsp;
+            </td>
+
+
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="Label_designer" runat="server" Text="Designer"></asp:Label>
+            </td>
+            <td>
+                <asp:DropDownList ID="DropDownList_designer" runat="server" DataSourceID="SqlDataSource1"
+                    DataTextField="D_Navn" DataValueField="D_id"  AppendDataBoundItems="true">
+                <asp:ListItem Value="0">-- Alle --</asp:ListItem></asp:DropDownList>
+                <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>"
+                    SelectCommand="SELECT * FROM [Design]"></asp:SqlDataSource>
+            </td>
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td class="style2">
+                <asp:Label ID="Label_design_aar" runat="server" Text="Design år"></asp:Label>
+            </td>
+            <td class="style2">
+                Min
+                <asp:TextBox ID="TextBox_designer_min" runat="server" MaxLength="4" 
+                    Width="35px"></asp:TextBox>
+                &nbsp; max
+                <asp:TextBox ID="TextBox_designer_max" runat="server" MaxLength="4" 
+                    Width="35px"></asp:TextBox>
+            </td>
+            <td class="style2">
+            </td>
+        </tr>
+        <tr>
+            <td class="style2">
+                <asp:Label ID="Label_pris" runat="server" Text="Pris"></asp:Label>
+            </td>
+            <td class="style2">
+                Min
+                <asp:TextBox ID="TextBox_pris_min" runat="server" MaxLength="100" Width="45px"></asp:TextBox>
+                &nbsp; max
+                <asp:TextBox ID="TextBox_pris_max" runat="server" MaxLength="100" Width="45px"></asp:TextBox>
+            </td>
+            <td class="style2">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                &nbsp;
+            </td>
+            <td>
+                &nbsp;
+            </td>
+            <td>
+                <asp:Button ID="Button_main_soeg" runat="server" Text="Søg" OnClick="Button_main_soeg_Click" />
+            </td>
+        </tr>
+    </table>
+    <section>
+        <article>
+            <asp:Repeater ID="Repeater_mobler_soeg_resultat" runat="server">
+            <HeaderTemplate>
+            <h1>Møbler</h1>
+            </HeaderTemplate>
+            <ItemTemplate>
+            <%#Eval("M_Navn") %> - fk_kat: <%#Eval("FK_Kategori") %> - FK_Designer: <%#Eval("FK_Design") %><hr />
+            </ItemTemplate>
+            </asp:Repeater>
+        <br />
+        </article>
+    <asp:SqlDataSource ID="SqlDataSource3" runat="server" 
+        ConnectionString="<%$ ConnectionStrings:ConnectionString %>" 
+        SelectCommand="SELECT Mobler.M_Navn, Design.D_Navn, Moble_Billeder.B_Navn, Kategori.K_Navn FROM Mobler INNER JOIN Design ON Mobler.FK_Design = Design.D_id INNER JOIN Moble_Billeder ON Mobler.M_id = Moble_Billeder.FK_Mobler INNER JOIN Kategori ON Mobler.FK_Kategori = Kategori.K_id WHERE (Mobler.FK_Kategori = @kategori) OR (Mobler.FK_Kategori = @kategori1) AND (Mobler.FK_Design = @designer) AND (Mobler.M_Design_aar BETWEEN @design_arr_min AND @design_arr_max) AND (Mobler.M_Pris BETWEEN @Design_pris_min AND @Design_pris_max)">
+        <SelectParameters>
+            <asp:Parameter Name="kategori" />
+            <asp:Parameter Name="kategori1" />
+            <asp:Parameter Name="designer" />
+            <asp:Parameter Name="design_arr_min" />
+            <asp:Parameter Name="design_arr_max" />
+            <asp:Parameter Name="Design_pris_min" />
+            <asp:Parameter Name="Design_pris_max" />
+        </SelectParameters>
+    </asp:SqlDataSource>
+    <br />
+    </section>
+    <asp:SqlDataSource ID="SqlDataSource4" runat="server" 
+        ConnectionString="<%$ ConnectionStrings:ConnectionString %>" 
+        
+        SelectCommand="SELECT Mobler.M_Navn, Kategori.K_Navn, Mobler.FK_Kategori FROM Mobler INNER JOIN Kategori ON Mobler.FK_Kategori = Kategori.K_id WHERE (Mobler.FK_Kategori = @kategori) AND (Mobler.FK_Design = @designer) AND (Mobler.M_Design_aar BETWEEN @design_arr_min AND @design_arr_max) AND (Mobler.M_Pris BETWEEN @Design_pris_min AND @Design_pris_max) OR (Mobler.FK_Kategori = @kategori1) AND (Mobler.FK_Design = @designer) AND (Mobler.M_Design_aar BETWEEN @design_arr_min AND @design_arr_max) AND (Mobler.M_Pris BETWEEN @Design_pris_min AND @Design_pris_max)">
+        <SelectParameters>
+            <asp:Parameter Name="kategori" />
+            <asp:Parameter Name="designer" />
+            <asp:Parameter Name="design_arr_min" />
+            <asp:Parameter Name="design_arr_max" />
+            <asp:Parameter Name="Design_pris_min" />
+            <asp:Parameter Name="Design_pris_max" />
+            <asp:Parameter Name="kategori1" />
+        </SelectParameters>
+    </asp:SqlDataSource>
+</asp:Content>
---
+++ Avanceret søg/AvanceretSoeg-CMK-Mobler/Soeg_Mobler.aspx.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+public partial class Soeg_Mobler : System.Web.UI.Page
+{
+
+    #region Beskrivelse
+    
+    
+    // Oprettelse af Avanceret søge sql streng
+    // Opgaven er løst ved brug af søgeformularen i CMK-Møbler opgaven
+    // 1. Opret først din søgestreng, som den skal se ud hvis den ikke er dynamisk.
+    //      Lav den som håndkode, eller brug SQL Query Builder
+    // 2. søgestrengen kommer til at se ud som noget ala dette
+    //
+    //  SELECT * FROM Mobler
+    //  WHERE     (FK_Kategori = @kategori) AND (FK_Design = @designer) AND 
+    //  (M_Design_aar BETWEEN @design_arr_min AND @design_arr_max) AND (M_Pris BETWEEN 
+    //  @Design_pris_min AND @Design_pris_max) 
+    //  OR 
+    //  (FK_Kategori = @kategori1) AND (FK_Design = @designer) AND 
+    //  (M_Design_aar BETWEEN @design_arr_min AND @design_arr_max) AND (M_Pris BETWEEN 
+    //  @Design_pris_min AND @Design_pris_max)
+    
+    // Brug evt. også inner join
+
+    // Det der ændres dynamisk er hvad der står efter "WHERE"
+    // parametrerne bliver kun tilføjet hvis der er skrevet noget i tekstfelterne, ellers spriges der videre til næste tekstfelt.
+    // Mellem hvert nyt parameter tilføjes der et 'AND' til søgestrengen, og herefter selve sql strengen
+    // Der tilføjes nyt til variablen 'sqlText' (som indeholder vores sql streng (SELECT * FROM Mobler WHERE )) ved hjælp af 
+    // += ""; (sqlText += " (FK_Kategori = @kategori)" ) 
+
+    // -----------------------------------------------------------------------------
+
+    #endregion
+
+    #region Private Fields (variabler der bruges i sidens metoder)
+
+    private SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+    private SqlCommand cmd = new SqlCommand();
+
+    private string sqlText = "SELECT * FROM Mobler WHERE";
+    private int count = 0;
+
+    #endregion
+
+    #region pageload
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    // Tilføjelse af Tomt felt til Dropdownlistens top
+    //protected void DropDownList_designer_DataBound(object sender, EventArgs e)
+    //{
+    //    DropDownList_designer.Items.Insert(0, new ListItem("- Alle -", "0"));
+    //}
+
+    #endregion
+
+    #region Søgning på varenummer med brug af LIKE
+
+
+    protected void Button_varenummer_Click(object sender, EventArgs e)
+    {
+        this.cmd.Connection = this.conn;
+        string nummer = TextBox_varenummer.Text;
+        this.cmd.CommandText = "SELECT * FROM Mobler WHERE (M_nummer LIKE '%" + @nummer + "%')";
+        cmd.Parameters.Add("@nummer", SqlDbType.NVarChar).Value = TextBox_varenummer.Text;
+
+        conn.Open();
+        Repeater_mobler_soeg_resultat.DataSource = cmd.ExecuteReader();
+        Repeater_mobler_soeg_resultat.DataBind();
+        conn.Close();
+    }
+
+    #endregion
+
+    #region Avanceret Søgning
+
+
+    protected void Button_main_soeg_Click(object sender, EventArgs e)
+    {
+        this.cmd.Connection = conn;
+
+        //CheckBoxList - Kategorier
+        if (CheckBoxList_kategori.SelectedIndex != -1)
+        {
+            // For hver checkbox der er afkrydset køres loopet
+            for (int i = 0; i < CheckBoxList_kategori.Items.Count; i++)
+            {
+                // Checkbokse
+                if (CheckBoxList_kategori.Items[i].Selected && this.count == 0)
+                {
+                    this.count = this.count + 1;
+                    this.cmd.CommandText = sqlText += " (FK_Kategori = @kategori)";
+                    cmd.Parameters.Add("@kategori", SqlDbType.Int).Value = CheckBoxList_kategori.Items[i].Value;
+
+                    //DropDownList - Designer
+                    DesignerMethod();
+                    // Design år
+                    DesignAarMethod();
+                    // Pris
+                    PrisMethod();
+                }
+                else if (CheckBoxList_kategori.Items[i].Selected)
+                {
+                    this.cmd.CommandText = sqlText += " OR (FK_Kategori = @kategori" + i + ")";
+                    cmd.Parameters.Add("@kategori" + i, SqlDbType.Int).Value = CheckBoxList_kategori.Items[i].Value;
+
+                    //DropDownList - Designer
+                    DesignerMethod();
+                    // Design år
+                    DesignAarMethod();
+                    // Pris
+                    PrisMethod();
+                }
+            }
+
+        }
+        else
+        {
+            //DropDownList - Designer
+            DesignerMethod();
+            // Design år
+            DesignAarMethod();
+            // Pris
+            PrisMethod();
+        }
+
+        // Default søg, hvis ingen felter er udfyldt
+        DefaultMethod();
+
+        conn.Open();
+        Repeater_mobler_soeg_resultat.DataSource = cmd.ExecuteReader();
+        Repeater_mobler_soeg_resultat.DataBind();
+        conn.Close();
+    }
+
+    #endregion
+
+    #region Private Metoder
+    // Private Metoder
+
+    private void DesignerMethod()
+    {
+        if (DropDownList_designer.SelectedIndex != 0)
+        {
+            SqlAndMethod();
+
+            this.count = this.count + 1;
+            this.cmd.CommandText = sqlText += " (FK_Design = @designer" + this.count + ")";
+            cmd.Parameters.Add("@designer" + this.count, SqlDbType.Int).Value = DropDownList_designer.SelectedValue;
+
+        }
+    }
+
+    private void DesignAarMethod()
+    {
+        if (TextBox_designer_min.Text != "" && TextBox_designer_max.Text != "")
+        {
+            SqlAndMethod();
+
+            this.count = this.count + 1;
+            this.cmd.CommandText = sqlText += " (M_Design_aar BETWEEN @design_arr_min" + this.count + " AND @design_arr_max" + this.count + ")";
+            cmd.Parameters.Add("@design_arr_min" + this.count, SqlDbType.Int).Value = TextBox_designer_min.Text;
+            cmd.Parameters.Add("@design_arr_max" + this.count, SqlDbType.Int).Value = TextBox_designer_max.Text;
+
+        }
+    }
+
+    private void PrisMethod()
+    {
+        if (TextBox_pris_min.Text != "" && TextBox_pris_max.Text != "")
+        {
+            SqlAndMethod();
+
+            this.count = this.count + 1;
+            this.cmd.CommandText = sqlText += " (M_Pris BETWEEN @Design_pris_min" + this.count + " AND @Design_pris_max" + this.count + ")";
+            cmd.Parameters.Add("@Design_pris_min" + this.count, SqlDbType.Int).Value = TextBox_pris_min.Text;
+            cmd.Parameters.Add("@Design_pris_max" + this.count, SqlDbType.Int).Value = TextBox_pris_max.Text;
+
+        }
+    }
+
+    private void SqlAndMethod()
+    {
+        if (this.count != 0)
+        {
+            this.cmd.CommandText = sqlText += " AND";
+
+        }
+    }
+
+    private void DefaultMethod()
+    {
+        if (CheckBoxList_kategori.SelectedIndex == -1 && DropDownList_designer.SelectedIndex == 0
+            && TextBox_designer_min.Text == "" && TextBox_designer_max.Text == ""
+            && TextBox_pris_min.Text == "" && TextBox_pris_max.Text == "")
+        {
+            this.cmd.CommandText = "SELECT * FROM Mobler";
+
+        }
+    }
+
+    #endregion
+
+
+}
